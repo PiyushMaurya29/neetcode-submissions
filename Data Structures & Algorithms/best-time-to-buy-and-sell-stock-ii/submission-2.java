205abf9e7fc3bf6ed1b752963019d82347c6362f
Piyush Maurya <piyushmaurya.cs@gmail.com>
2026-08-17 19:16:08 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int solve(int index, int[] prices, int canBuy, int[][] dp){
+        if(index >= prices.length) return 0;
+        if(dp[index][canBuy] != -1){
+            return dp[index][canBuy];
+        }
+        if(canBuy == 1){
+            int buy = -prices[index] + solve(index+1, prices, 0, dp);
+            int notBuy = solve(index+1, prices, 1, dp);
+            return dp[index][canBuy] = Math.max(buy, notBuy);
+        }
+        else{
+            int sell = prices[index] + solve(index+1, prices, 1, dp);
+            int notSell = solve(index+1, prices, 0, dp);
+            return dp[index][canBuy] = Math.max(sell, notSell);
+        }
+    }
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+
+
+        // int[][] dp = new int[prices.length+1][2];
+        // for(int[] row : dp){
+        //     Arrays.fill(row, -1);
+        // }
+        // return solve(0, prices, 1, dp);
+    }
+}
