95872925b486300f9edba9d5b496e54c258609b9
Piyush Maurya <piyushmaurya.cs@gmail.com>
2026-08-30 02:09:56 +0530

    Add: wiggle-sort - submission-0

---
+++ Data Structures & Algorithms/wiggle-sort/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int index = n-1;
+        for(int i=1 ; i<n ; i+=2){
+            result[i] = nums[index--];
+        }
+        index = 0;
+        for(int i=0 ; i<n ; i+=2){
+            result[i] = nums[index++];
+        }
+        for(int i=0 ; i<n ; i++){
+            nums[i] = result[i];
+        }
+    }
+}
