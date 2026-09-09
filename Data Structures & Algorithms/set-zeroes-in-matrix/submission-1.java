49be459d3f64e5d5a7d458e2d71ce7f54fae9e43
Piyush Maurya <piyushmaurya.cs@gmail.com>
2026-09-09 14:59:26 +0530

    Add: set-zeroes-in-matrix - submission-1

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-1.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void markMatrix(int r, int c, int row, int col, boolean[][] mark){
+        for(int j=0 ; j<c ; j++){
+            mark[row][j] = true;
+        }
+        for(int i=0 ; i<r ; i++){
+            mark[i][col] = true;
+        }
+    }
+    public void setZeroes(int[][] matrix) {
+        // int r = matrix.length;
+        // int c = matrix[0].length;
+
+        // boolean[] rowHasZero = new boolean[r];
+        // boolean[] columnHasZero = new boolean[c];
+
+        // for(int i=0 ; i<r ; i++){
+        //     for(int j=0 ; j<c ; j++){
+        //         if(matrix[i][j] == 0){
+        //             rowHasZero[i] = true;
+        //             columnHasZero[j] = true;
+        //         }
+        //     }
+        // }
+
+        // for(int i=0 ; i<r ; i++){
+        //     for(int j=0 ; j<c ; j++){
+        //         if(rowHasZero[i] || columnHasZero[j]){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+
+        int r = matrix.length;
+        int c = matrix[0].length;
+
+        boolean[][] mark = new boolean[r][c];
+        for(int i=0 ; i<r ; i++){
+            for(int j=0 ; j<c ; j++){
+                if(matrix[i][j] == 0){
+                    markMatrix(r, c, i, j, mark);
+                }
+            }
+        }
+        for(int i=0 ; i<r ; i++){
+            for(int j=0 ; j<c ; j++){
+                if(mark[i][j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
